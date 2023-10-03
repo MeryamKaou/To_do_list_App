930c660d8edd00a4aa56f7ea98c93be1ab17e1f5
Kaou Merii <87898725+MeryamKaou@users.noreply.github.com>
2023-10-03 17:48:09 +0100

    add js file

---
+++ script.js
@@ -0,0 +1,18 @@
+const todoFrom =document.querySelector("#todo-form");
+const todoInput =document.querySelector("#todo-input");
+const tasksList =document.querySelector("#tasks");
+
+todoFrom.addEventListener("submit", function(e)
+    {
+        e.preventDefault();
+        if(todoInput.value === "")
+            {
+                    alert("please enter a task ");
+                    return;
+            }
+
+        const task = document.createElement("li");
+        task.innerHTML = todoInput.value;
+        tasksList.appendChild(task);
+        todoInput.value="";
+    });
