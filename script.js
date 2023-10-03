ef5e449277b23f23059c2fe3f80caef55fdc0e46
Kaou Merii <87898725+MeryamKaou@users.noreply.github.com>
2023-10-03 18:18:32 +0100

    done

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const todoFrom =document.querySelector("#todo-form");
-const todoInput =document.querySelector("#todo-input");
-const tasksList =document.querySelector("#tasks");
+const todoFrom = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const tasksList = document.querySelector("#tasks");
 
-todoFrom.addEventListener("submit", function(e)
-    {
-        e.preventDefault();
-        if(todoInput.value === "")
-            {
-                    alert("please enter a task ");
-                    return;
-            }
+todoFrom.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (todoInput.value === "") {
+    alert("please enter a task ");
+    return;
+  }
 
-        const task = document.createElement("li");
-        task.innerHTML = todoInput.value;
-        tasksList.appendChild(task);
-        todoInput.value="";
-    });
+  const task = document.createElement("li");
+  task.innerHTML = todoInput.value;
+  tasksList.appendChild(task);
+  todoInput.value = "";
+});
